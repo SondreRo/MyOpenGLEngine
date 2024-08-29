32d56559cf98f31f1464b0238fd2de58fdc67f24
Sondre R <soroensg@gmail.com>
2024-08-29 09:15:06 +0200

    Cmake Commit

---
+++ MyOpenGLEngine/MyOpenGLEngine.cpp
@@ -0,0 +1,12 @@
+﻿// MyOpenGLEngine.cpp : Defines the entry point for the application.
+//
+
+#include "MyOpenGLEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
